6acf6c2e0f0e58046d6ce6850693faecf80ca509
HerumbNagpal <66248817+HerumbNagpal@users.noreply.github.com>
2023-07-16 15:58:52 +0530

    Updating gh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import Github from '../assets/images/gh.png'
 import Insta from '../assets/images/instagram.png'
 import LinkedIn from '../assets/images/linkedIn.png'
 
 export const Footer = () => {
   return (
     <div className='footer'>
-        <div className="imgs">
-            <a href="https://www.instagram.com/herumb_nagpal/" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'inherit' }}  > <img className='imge' src={Insta} alt="ig" />  </a>  
-            <a href="https://www.linkedin.com/in/herumb-nagpal-a37b94192/" target="_blank"  rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'inherit' }}  > <img className='imge' src={LinkedIn} alt="linkedin" /> </a> 
-        </div>
-        <div className="year">
-            <p>© 2023  Herumb Nagpal</p>
-        </div>
+      <div className="imgs">
+      <a href="https://github.com/HerumbNagpal" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'inherit' }}  > <img className='imge' src={Github} alt="ig" />  </a>
+        <a href="https://www.instagram.com/herumb_nagpal/" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'inherit' }}  > <img className='imge' src={Insta} alt="ig" />  </a>
+        <a href="https://www.linkedin.com/in/herumb-nagpal-a37b94192/" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'inherit' }}  > <img className='imge' src={LinkedIn} alt="linkedin" /> </a>
+      </div>
+      <div className="year">
+        <p>© 2023  Herumb Nagpal</p>
+      </div>
     </div>
   )
 }
