93a1fd1020ef9e5eb94a70e268130217d74af21e
HerumbNagpal <66248817+HerumbNagpal@users.noreply.github.com>
2023-10-06 13:15:03 +0530

    trying to fix apk doqnload issues

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -91,7 +91,7 @@ export const Projects = () => {
                             A mobile application which can be used to perform mathematical calulations. <br/> The app was developed in React-Native framework along with Redux.
                         </p>
                         <br /><br /><br />
-                        <a style={{ color: 'inherit', textDecoration: 'inherit' }} href="/apk/myCalculator.apk"  download>Click here to download and install the apk . </a>
+                        <a style={{ color: 'inherit', textDecoration: 'inherit' }} href="/apk/myCalculator.apk"  download='myCalculator.apk'>Click here to download and install the apk . </a>
                     </div>
                     <div className="pScreenshot">
                         <img src={calculator} height="300px" width="300px" alt="" />
